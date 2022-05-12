6a7dd8087fda0c5960802ad1a8b77ea091e7ccb8
rohitprofc <rohitprofc@gmail.com>
2022-05-12 20:40:33 +0530

    May 12

--- 6.c
+++ 6.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 void main()
 {
-    int a[100], i, j, n, el;
+    int a[100], i, j, n, key;
     printf("Enter no.of elements: ");
     scanf("%d", &n);
     printf("Enter elements into array: \n");
@@ -15,21 +15,34 @@ void main()
     printf("Elements after sorting:\n");
     for (i = 0; i < n; i++)
     {
-        el = a[i];
+        key = a[i];
         j = i - 1;
-        while (j >= 0 && a[j] > el)
+        while (j >= 0 && a[j] > key)
         {
             a[j + 1] = a[j];
             j--;
         }
-        a[j + 1] = el;
+        a[j + 1] = key;
     }
     for (i = 0; i < n; i++)
     {
         printf("Element - %d: %d\n", i, a[i]);
     }
 }
+
 /*
 Output:-
-
+Enter no.of elements: 5
+Enter elements into array: 
+Element - 0: 90
+Element - 1: 10
+Element - 2: 80
+Element - 3: 20
+Element - 4: 70
+Elements after sorting:
+Element - 0: 10
+Element - 1: 20
+Element - 2: 70
+Element - 3: 80
+Element - 4: 90
 */
