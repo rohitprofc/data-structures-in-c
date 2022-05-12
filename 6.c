873a29b0375e7b825af0aa5795f3ea8427283006
rohitprofc <rohitprofc@gmail.com>
2022-05-12 20:37:20 +0530

    May 12

--- 6.c
+++ 6.c
@@ -12,20 +12,18 @@ void main()
         printf("Element - %d: ", i);
         scanf("%d", &a[i]);
     }
-    /*
     printf("Elements after sorting:\n");
     for (i = 0; i < n; i++)
     {
         el = a[i];
         j = i - 1;
-        while (j >= 0 && a[j > el])
+        while (j >= 0 && a[j] > el)
         {
             a[j + 1] = a[j];
             j--;
         }
         a[j + 1] = el;
     }
-    */
     for (i = 0; i < n; i++)
     {
         printf("Element - %d: %d\n", i, a[i]);
