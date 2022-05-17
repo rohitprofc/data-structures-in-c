d7cd8de3868545f763685e25b01d9998d762666c
rohitprofc <rohitprofc@gmail.com>
2022-05-17 21:43:27 +0530

    May 17

--- a.c
+++ a.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     printf("Hola!\nI'm Rohit\nI play chess\nwhat about a game??");
     return 0;
 }
-/* 
-Output:- 
+/*
+Output:-
 Hola!
 I'm Rohit
 I play chess
